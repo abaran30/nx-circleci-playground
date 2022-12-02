b3846f1c67a8e81eab079c8f8d9ceb88cb9f8c14
Adrian Baran <adbaran@cisco.com>
2022-12-02 08:03:00 -0600

    chore: reproduce okta-angular issue 110

--- apps/my-app-a/src/app/app.component.ts
+++ apps/my-app-a/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+
+import { OKTA_AUTH } from '@okta/okta-angular';
+import { OktaAuth } from '@okta/okta-auth-js';
 
 @Component({
   selector: 'nx-circleci-playground-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-app-a';
+  user: string = '';
+
+  constructor(@Inject(OKTA_AUTH) private oktaAuth: OktaAuth) { }
+
+  async ngOnInit() {
+    const user = await this.oktaAuth.getUser();
+    this.user = JSON.stringify(user, null, 4);
+  }
 }
--- apps/my-app-a/src/app/app.module.ts
+++ apps/my-app-a/src/app/app.module.ts
@@ -4,10 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
+import {
+  OKTA_CONFIG,
+  OktaAuthModule
+} from '@okta/okta-angular';
+import { OktaAuth } from '@okta/okta-auth-js';
+
+const config = {
+  issuer: 'https://{yourOktaDomain}/oauth2/default',
+  clientId: '{clientId}',
+  redirectUri: window.location.origin + '/login/callback'
+}
+const oktaAuth = new OktaAuth(config);
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
-  providers: [],
+  imports: [BrowserModule, OktaAuthModule],
+  providers: [
+    { 
+      provide: OKTA_CONFIG, 
+      useValue: { oktaAuth } 
+    }
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
